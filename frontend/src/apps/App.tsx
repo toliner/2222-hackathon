a82a14f9caf8ce95856405bc64ba036390e21be2
Rena <choco.rena0609@gmail.com>
2020-02-14 02:43:11 +0900

    routerと全pageの作成

--- frontend/src/apps/App.tsx
+++ frontend/src/apps/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {MenuAppBar} from "../components/common/menu-app-bar";
-import {Home} from "../components/pages/home";
+import {Router} from "../components/router";
 
 const App = () => {
     return (
         <div className="App">
             <MenuAppBar/>
-            <Home />
+            <Router />
         </div>
     );
 }
