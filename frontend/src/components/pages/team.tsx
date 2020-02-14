366a44cc54faffb34c38f7acdf306956c9839904
Rena <choco.rena0609@gmail.com>
2020-02-14 20:51:42 +0900

    format

--- frontend/src/components/pages/team.tsx
+++ frontend/src/components/pages/team.tsx
@@ -1,68 +1,68 @@
 import React from "react";
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
-import {Grid} from "@material-ui/core";
-import {TeamListCard} from "../partials/team-list-card";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import { TeamListCard } from "../partials/team-list-card";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1
-        },
-        paper: {
-            textAlign: "center",
-            background: "#F3F3F3",
-            height: "100%",
-            top: 68,
-            position: "relative",
-        },
-        bg: {
-            background: "#4B4B4B",
-        },
-        bottom: {
-            marginBottom: 25,
-            display: "flex",
-        },
-        space: {
-            margin: 10,
-        }
-    })
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      textAlign: "center",
+      background: "#F3F3F3",
+      height: "100%",
+      top: 68,
+      position: "relative"
+    },
+    bg: {
+      background: "#4B4B4B"
+    },
+    bottom: {
+      marginBottom: 25,
+      display: "flex"
+    },
+    space: {
+      margin: 10
+    }
+  })
 );
 
 export const Team: React.FC = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={3} className={classes.bg}/>
-                <Grid item xs={6} className={classes.paper}>
-                    <div className={classes.bottom}>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                    </div>
-                    <div className={classes.bottom}>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                    </div>
-                    <div className={classes.bottom}>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                        <Grid item xs={6} className={classes.space}>
-                            <TeamListCard/>
-                        </Grid>
-                    </div>
-                </Grid>
-                <Grid item xs={3} className={classes.bg}/>
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3} className={classes.bg} />
+        <Grid item xs={6} className={classes.paper}>
+          <div className={classes.bottom}>
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
             </Grid>
-        </div>
-    )
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
+            </Grid>
+          </div>
+          <div className={classes.bottom}>
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
+            </Grid>
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
+            </Grid>
+          </div>
+          <div className={classes.bottom}>
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
+            </Grid>
+            <Grid item xs={6} className={classes.space}>
+              <TeamListCard />
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item xs={3} className={classes.bg} />
+      </Grid>
+    </div>
+  );
 };
--- frontend/src/components/partials/tournament-list-card.tsx
+++ frontend/src/components/partials/tournament-list-card.tsx
@@ -1,41 +1,44 @@
 import React from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import {Button, Card, CardContent, CardMedia, Typography} from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            background: "#E1E1E1",
-            textAlign: "left",
-            height: 220,
-        },
-        media: {
-            height: 140,
-        },
-        title: {
-            fontSize: 14,
-        },
-        btn:{
-            textAlign: "center",
-            marginTop: 20,
-        }
-    })
+  createStyles({
+    root: {
+      flexGrow: 1,
+      background: "#E1E1E1",
+      textAlign: "left",
+      height: 220
+    },
+    media: {
+      height: 140
+    },
+    title: {
+      fontSize: 14
+    },
+    btn: {
+      textAlign: "center",
+      marginTop: 20
+    }
+  })
 );
 
 export const TournamentListCard: React.FC = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography variant="h6">開催中の大会</Typography>
-                <Typography variant="subtitle2">2020/02/22 10:00 ~ </Typography>
-                <CardMedia
-                    className={classes.media}
-                    image="icon"
-                />
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h6">開催中の大会</Typography>
+        <Typography variant="subtitle2">2020/02/22 10:00 ~ </Typography>
+        <CardMedia className={classes.media} image="icon" />
+      </CardContent>
+    </Card>
+  );
 };
