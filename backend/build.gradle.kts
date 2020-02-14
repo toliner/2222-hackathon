c578762e08d6379652bc840b6fd41068672242a6
toliner <toliner.rkt@gmail.com>
2020-02-15 02:44:49 +0900

    ktorのセットアップ

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    application
 }
 
 group = "org.example"
@@ -13,13 +14,20 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation(kotlin("test-junit"))
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 
     implementation(ktorServerCore())
     implementation(ktorJackson())
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-html-builder:$ktor_version")
+    implementation("io.ktor:ktor-locations:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:1.2.3")
 
     implementation(jacksonDataFormatXml())
 
@@ -33,17 +41,28 @@ dependencies {
     implementation(exposed())
     implementation(hikariCp())
     guava()
+
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlin.Experimental"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlin.Experimental"
     }
 
     test {
         useJUnitPlatform()
+    }
+
+    application {
+        mainClassName = "io.ktor.server.netty.EngineMain"
     }
 }
--- backend/src/main/kotlin/app/reiwa/hackathon/Main.kt
+++ backend/src/main/kotlin/app/reiwa/hackathon/Main.kt
@@ -1,5 +1,45 @@
 package app.reiwa.hackathon
 
-fun main() {
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.serialization.serialization
+import io.ktor.util.KtorExperimentalAPI
+import org.slf4j.event.Level
+
+@UseExperimental(KtorExperimentalAPI::class)
+fun Application.mainModule() {
+    install(DefaultHeaders)
+    install(CallLogging) {
+        level = Level.INFO
+    }
+    install(ContentNegotiation) {
+        serialization()
+    }
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) {
+            call.respondText("404", status = HttpStatusCode.NotFound)
+        }
+    }
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+        get("/aa") {
+            call.respondText("aa")
+        }
+        get("/snippets") {
+            call.respond(mapOf("OK" to true))
+        }
+    }
 
 }
