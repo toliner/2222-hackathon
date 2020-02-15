2fc0ecde34664c3c5dfc839a6e95543372fe8ddb
Rena <choco.rena0609@gmail.com>
2020-02-15 20:30:14 +0900

    drawerの作成

--- frontend/src/components/common/menu-app-bar.tsx
+++ frontend/src/components/common/menu-app-bar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -10,6 +9,15 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 //import { withRouter } from 'react-router';
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import AddIcon from "@material-ui/icons/Add";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,26 +25,48 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1
     },
     appBar: {
-      backgroundColor: "rgba(58, 58, 58, 1)"
+      backgroundColor: "#3A3A3A"
     },
     menuButton: {
       marginRight: theme.spacing(2)
     },
     title: {
       flexGrow: 1
+    },
+    subtitle: {
+      color: "#979797",
+      marginLeft: 15
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundColor: "#3A3A3A",
+      color: "#FFFFFF"
+    },
+    drawerItem: {
+      display: "block"
+    },
+    closeBtn: {
+      float: "right",
+      color: "#FFFFFF"
     }
   })
 );
 
 export const MenuAppBar: React.FC = () => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const open = Boolean(anchorEl);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) =>
     setAnchorEl(event.currentTarget);
-
   const handleClose = () => setAnchorEl(null);
+  const handleDrawerOpen = () => setDrawerOpen(true);
+  const handleDrawerClose = () => setDrawerOpen(false);
 
   /*const handleToUserPage = () => {
     props.history.push("/user");
@@ -51,6 +81,7 @@ export const MenuAppBar: React.FC = () => {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
@@ -96,6 +127,41 @@ export const MenuAppBar: React.FC = () => {
           </div>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={drawerOpen}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div>
+          <IconButton onClick={handleDrawerClose} className={classes.closeBtn}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Typography variant="subtitle2" className={classes.subtitle}>
+          create
+        </Typography>
+        <List>
+          {[" + TOURNAMENT", " + TEAM"].map(text => (
+            <ListItem button key={text} className={classes.drawerItem}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Typography variant="subtitle2" className={classes.subtitle}>
+          channel
+        </Typography>
+        <List>
+          {[" # DASHBOARD", " # TEAM", "# TOURNAMENT"].map(text => (
+            <ListItem button key={text} className={classes.drawerItem}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 };
