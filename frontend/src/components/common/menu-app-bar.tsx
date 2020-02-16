5e8c423db07ca43e7e99d442a247574e0642a551
shuta13 <hollystarsun@gmail.com>
2020-02-16 18:59:20 +0900

    fix: 直したけど多分アンチパターン

--- frontend/src/components/common/menu-app-bar.tsx
+++ frontend/src/components/common/menu-app-bar.tsx
@@ -13,7 +13,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, useHistory } from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -88,12 +88,49 @@ export const MenuAppBar: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [drawerOpen, setDrawerOpen] = useState(false);
   const open = Boolean(anchorEl);
+  const [updatePage, setUpdatePage] = useState(false);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) =>
     setAnchorEl(event.currentTarget);
   const handleClose = () => setAnchorEl(null);
   const handleDrawerOpen = () => setDrawerOpen(true);
   const handleDrawerClose = () => setDrawerOpen(false);
+
+  const LinkLists = () => {
+    const history = useHistory();
+    const linkToPath = (path: string) => {
+      history.push(path);
+      window.location.reload();
+    }
+    return (
+      <div>
+        <Typography variant="subtitle2" className={classes.subtitle}>
+          create
+        </Typography>
+        <List>
+          {navListTop.map((text, index) => (
+            <ListItem button key={index} className={classes.drawerItem}>
+              <div onClick={() => linkToPath(text.path)} className={classes.link}>
+                <ListItemText primary={text.name} />
+              </div>
+            </ListItem>
+          ))}
+        </List>
+        <Typography variant="subtitle2" className={classes.subtitle}>
+          channel
+        </Typography>
+        <List>
+          {navListBottom.map((text, index) => (
+            <ListItem button key={index} className={classes.drawerItem}>
+              <div onClick={() => linkToPath(text.path)} className={classes.link}>
+                <ListItemText primary={text.name} />
+              </div>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+  }
 
   return (
     <div className={classes.root}>
@@ -166,31 +203,8 @@ export const MenuAppBar: React.FC = () => {
             <ChevronLeftIcon />
           </IconButton>
         </div>
-        <Typography variant="subtitle2" className={classes.subtitle}>
-          create
-        </Typography>
         <Router>
-          <List>
-            {navListTop.map((text, index) => (
-              <ListItem button key={index} className={classes.drawerItem}>
-                <Link to={text.path} className={classes.link}>
-                  <ListItemText primary={text.name} />
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-          <Typography variant="subtitle2" className={classes.subtitle}>
-            channel
-          </Typography>
-          <List>
-            {navListBottom.map((text, index) => (
-              <ListItem button key={index} className={classes.drawerItem}>
-                <Link to={text.path} className={classes.link}>
-                  <ListItemText primary={text.name} />
-                </Link>
-              </ListItem>
-            ))}
-          </List>
+          <LinkLists />
         </Router>
       </Drawer>
     </div>
