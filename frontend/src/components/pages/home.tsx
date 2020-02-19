2f8eec1393537e50f2d90d929bbeff1284de2e20
shuta13 <hollystarsun@gmail.com>
2020-02-20 01:55:06 +0900

    fix: homeだいぶゴリゴリやった

--- frontend/src/components/pages/home.tsx
+++ frontend/src/components/pages/home.tsx
@@ -3,6 +3,7 @@ import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import { Button, Grid, Typography, Box } from "@material-ui/core";
 import logo from "../../static/logo.png";
 import demo from "../../static/demo.jpg";
+import { VFXProvider, VFXImg } from "react-vfx";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,45 +11,71 @@ const useStyles = makeStyles((theme: Theme) =>
       background: "#4B4B4B",
       margin: 0,
       padding: 0,
+      width: "100vw",
       height: "100vh",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
       overflow: "hidden"
     },
+    wrap: {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap"
+    },
     paper: {
-      textAlign: "center",
-      background: "#4B4B4B"
+      width: 400,
+      background: "#4B4B4B",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap: "wrap",
+      margin: "2vw" // これだいぶゴリってるのでたまたま
     },
     bottom: {
-      marginBottom: 25,
       display: "flex"
     },
     btn: {
       textAlign: "center",
       backgroundColor: "#FFFFFF",
       borderRadius: 30,
-      width: 238,
-      height: 64
+      width: "80vw",
+      maxWidth: 240,
+      height: 64,
+      marginTop: 16
     },
     link: {
       textDecoration: "none",
       color: "#5D5D5D"
     },
-    imageClip: {
+    imageWrap: {
+      width: "90vw",
+      maxWidth: 400,
       display: "flex",
       justifyContent: "center",
-      alignItems: "center",
-      overflow: "hidden"
+      alignItems: "center"
     },
-    image: {
-      width: 520
+    logo: {
+      width: "90vw",
+      maxWidth: 400
     },
     demo: {
-      width: 520
+      width: "90vw",
+      maxWidth: 400
     },
     description: {
-      color: "#F2F2F2"
+      color: "#F2F2F2",
+      fontSize: 20,
+      textAlign: "center"
+    },
+    subDescription: {
+      color: "#F2F2F2",
+      textAlign: "center",
+      fontSize: 14,
+      marginBottom: 40,
+      lineHeight: 1
     }
   })
 );
@@ -70,28 +97,39 @@ export const Home = () => {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={6} className={classes.paper}>
-          <div className={classes.imageClip}>
-            <Box boxShadow={4}>
-              <img src={demo} alt="logo" className={classes.demo} />
-            </Box>
+        <div className={classes.wrap}>
+          <div className={classes.paper}>
+            <div className={classes.imageWrap}>
+              <Box boxShadow={4}>
+                <img src={demo} alt="logo" className={classes.demo} />
+              </Box>
+            </div>
+            <div className={classes.wrap}>
+              <p className={classes.description}>
+                もっと気軽にe-Sportsを楽しめる！
+              </p>
+              <div className={classes.subDescription}>
+                <p>チーム作成・大会作成</p>
+                <p>大会トーナメント機能</p>
+                <p>動向が見やすいタイムライン</p>
+              </div>
+            </div>
           </div>
-          <div className={classes.description}>
-            e-Sportsを気軽に楽しめる！
+          <div className={classes.paper}>
+            <div className={classes.imageWrap}>
+              <VFXProvider>
+                <VFXImg src={logo} alt="logo" shader="rgbShift" className={classes.logo} />
+              </VFXProvider>
+            </div>
+            <Typography>
+              <Button variant="contained" className={classes.btn}>
+                <a href="./login" className={classes.link}>
+                  新規登録
+                </a>
+              </Button>
+            </Typography>
           </div>
-        </Grid>
-        <Grid item xs={6} className={classes.paper}>
-          <div className={classes.imageClip}>
-            <img src={logo} alt="logo" className={classes.image} />
-          </div>
-          <Typography>
-            <Button variant="contained" className={classes.btn}>
-              <a href="./login" className={classes.link}>
-                新規登録
-              </a>
-            </Button>
-          </Typography>
-        </Grid>
+        </div>
       </Grid>
     </div>
   );
